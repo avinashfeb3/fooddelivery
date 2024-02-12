39e073d8b7daa6d64cb7bfbb17a0e5fafce00328
DesignerLRS32 <designer@lrsservices.in>
2024-02-12 14:53:44 +0530

    Add more section in Home.jsx and add card.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import Layout from '../../components/Layouts/Layout';
-import Section1 from './Section1';
 import "../../styles/HomeStyle.css";
+import Section1 from './Section1';
+import Section2 from './Section2';
+import Section3 from './Section3';
+import Section4 from './Section4';
 function Home() {
   return (
     <>
       <Layout>
-        {/* Home Section Hero Banner */}
+        {/* Home Section Hero Banner Start */}
         <Section1/>
+        {/* Home Section Hero Banner End */}
+        
+        {/* Home Section About Start */}
+        <Section2/>
+        {/* Home Section About End */}
+
+        {/* Home Section Menu Start */}
+        <Section3/> 
+        {/* Home Section Menu End */}
+
+        {/* Home Section Promotion start */}
+        <Section4/>
+        {/* Home Section Promotion End */}
+
       </Layout>
     </>
   )
